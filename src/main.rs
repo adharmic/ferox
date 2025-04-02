175865110805d76b8f0ed0a4831be08241312690
Adithya Ajith <adithya@satyaloka.org>
2025-04-02 17:16:04 +0530

    Reposition spheres for better output result

--- src/main.rs
+++ src/main.rs
@@ -10,22 +10,21 @@ fn main() {
     let mut spheres = Vec::new();
     let mut lights = Vec::new();
     spheres.push(Sphere::new(
-        Vec3::new(-3f32, 0f32, -16f32),
+        Vec3::new(-3f32, 0f32, -8f32),
         2f32,
         Material::new(Rgb([255, 0, 0])),
     ));
     spheres.push(Sphere::new(
-        Vec3::new(-1f32, -1.5f32, -12f32),
+        Vec3::new(-1f32, -1.5f32, -6f32),
         2f32,
         Material::new(Rgb([0, 255, 0])),
     ));
     spheres.push(Sphere::new(
-        Vec3::new(1.5f32, -0.5f32, -18f32),
+        Vec3::new(1.5f32, -0.5f32, -9f32),
         2f32,
         Material::new(Rgb([0, 0, 255])),
     ));
 
-    lights.push(Light::new(Vec3::new(-20f32, -20f32, -20f32), 0.5));
-    lights.push(Light::new(Vec3::new(20f32, 10f32, -10f32), 1.5));
+    lights.push(Light::new(Vec3::new(5f32, 5f32, -2f32), 1.5));
     raytracer::render(&spheres, &lights);
 }
